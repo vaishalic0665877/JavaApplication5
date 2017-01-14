37d85126f9900bf1ebd7a4ad7d2e7bbd223f6025
Vaishali Somu Ramesh <Vaishali Somu Ramesh@192.168.0.74>
2017-01-13 23:59:14 -0500

    Created stringPower skeleton.

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -17,5 +17,10 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+        public static String stringPower(String name, int number){
+            
+            return "";
+        }
+    
     
 }
